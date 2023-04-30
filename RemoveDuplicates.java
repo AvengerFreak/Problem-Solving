72c9ec255702b07200ef81c2670cb65ae17e4aac
AvengerFreak <starwarsgeek14mz@gmail.com>
2023-04-30 18:56:36 -0500

    Update RemoveDuplicates.java
    Fix test case bug

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -3,28 +3,31 @@
 class RemoveDuplicates {
     public int removeDuplicates(int[] nums) {
         
-       int size = nums.length;
-        
-        if(size == 0 || size == 1 ){
-            return size;
-        }
-        
+       int n = arr.length;
+       // Return, if array is empty or
+        // contains a single element
+        if (n == 0 || n == 1)
+            return n;
+ 
+        int[] temp = new int[n];
+ 
+        // Start traversing elements
         int j = 0;
-        int temp[] = new int[size - 1];
-        for(int i = 0; i< size - 1; i++){
-            
-            if(nums[i] != nums[i+1]){
+        for (int i = 0; i < n - 1; i++)
+             
+            // If current element is not equal to next
+            // element then store that current element
+            if (nums[i] != nums[i + 1])
                 temp[j++] = nums[i];
-            }
-            
-        }
-        
-        temp[j++] = nums[size - 2];
-        
-        for(int i=0; i< j; i++ ){
+ 
+        // Store the last element as whether it is unique or
+        // repeated, it hasn't stored previously
+        temp[j++] = nums[n - 1];
+ 
+        // Modify original array
+        for (int i = 0; i < j; i++)
             nums[i] = temp[i];
-        }
-        
+ 
         return j;
     }
 }
