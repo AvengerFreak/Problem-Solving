caa0001c0ae89fea262fec1d3d6934aab99e516f
AvengerFreak <starwarsgeek14mz@gmail.com>
2023-05-05 22:39:51 -0500

    Create ReverseInteger.java
    Solution to leetcode problem https://leetcode.com/explore/featured/card/top-interview-questions-easy/127/strings/880/

---
+++ ReverseInteger.java
@@ -0,0 +1,30 @@
+class ReverseInteger {
+
+// solution to leetcode problem
+    public int reverse(int x) {
+        
+        StringBuilder strb = new StringBuilder();
+        strb.append(String.valueOf(x));
+        
+        if(x < 0){
+           strb.deleteCharAt(0);
+            
+            try{
+                return Integer.parseInt(strb.reverse().toString()) *-1;
+            }
+            
+            catch(Exception e){
+                return 0;
+            }
+            
+        }
+        
+        try{
+                return Integer.parseInt(strb.reverse().toString());
+            }
+            
+            catch(Exception e){
+                return 0;
+            }
+    }
+}
