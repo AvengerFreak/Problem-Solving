e834b356e80d19d4f59bad0c8b53a94af4f75002
AvengerFreak <starwarsgeek14mz@gmail.com>
2023-05-09 23:06:36 -0500

    Create IsPalindrome.java
    solution to leetcode problem https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/883/

---
+++ IsPalindrome.java
@@ -0,0 +1,9 @@
+class IsPalindrome {
+  
+  // solution to leetcode problem https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/883/
+    public boolean isPalindrome(String s) {
+        
+        StringBuilder sb = new StringBuilder(s.replaceAll("[^A-Za-z0-9]", "").toLowerCase());
+        return sb.toString().equals(sb.reverse().toString());
+    }
+}
